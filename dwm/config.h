ed7c3a2b82b2596f782d1f35063b2b87b5c3d7b9
Job <jobse@pm.me>
2020-08-25 13:13:29 +0200

    Removed firefox workspace 9

--- dwm/config.h
+++ dwm/config.h
@@ -26,7 +26,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   isterminal     noswallow   monitor */
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0, 0, -1},
 	{ "st-256color",NULL,     NULL,       0,            0,           1, 0, -1},
 };
 
